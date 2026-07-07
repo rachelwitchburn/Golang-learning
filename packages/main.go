d865b15b6e5cf479bed0781e653f807395a5b5ef
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2026-07-07 09:37:14 -0300

    Aprendizado sobre packages

---
+++ packages/main.go
@@ -0,0 +1,15 @@
+// Todo arquivo .go deve começar com package nome_do_pacote na primeira linha.
+
+package main // Deve conter a função main
+
+import "fmt"
+
+// Teste da função init
+
+func init() {
+	fmt.Print("Testando o init")
+}
+
+func main() {
+	// não precisa de algo dentro
+}
